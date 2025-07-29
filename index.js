e84ad077ba6831b83551bdf5a8516c2e17fdb24e
SantiagoGiustolisi <168446113+SantiagoGiustolisi@users.noreply.github.com>
2025-07-29 13:35:26 -0300

    todo cambiado ida

--- index.js
+++ index.js
@@ -1,119 +1,639 @@
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-// Datos de rutas
+// 🔥 TODAS LAS RUTAS COMPLETAS
 const rutas = [
   {
-    origen: "Parana",
-    destinos: [
-      { destino: "San Benito", hora_salida: "08:15", hora_llegada: "08:40", precio: 1500 },
-      { destino: "C Avellaneda", hora_salida: "08:15", hora_llegada: "09:00", precio: 2900 },
-      { destino: "Sauce Montrul", hora_salida: "08:15", hora_llegada: "09:15", precio: 4200 },
-      { destino: "Picadita", hora_salida: "08:15", hora_llegada: "09:30", precio: 5500 },
-      { destino: "La Balsa", hora_salida: "08:15", hora_llegada: "09:40", precio: 6500 },
-      { destino: "Urquiza", hora_salida: "08:15", hora_llegada: "10:00", precio: 8500 }
+    "origen": "Paraná",
+    "destinos": [
+      {
+        "destino": "San Benito",
+        "precio": 1700,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:00hs",
+            "12:00hs",
+            "19:00hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:30hs",
+            "18:30hs"
+          ],
+          "lunes_a_viernes_vuelta": [
+            "07:10hs",
+            "18:40hs"
+          ],
+          "sabados_domingos_vuelta": [
+            "20:55hs"
+          ],
+          "diario": [
+            "14:25hs"
+          ]
+        }
+      },
+      {
+        "destino": "Colonia Avellaneda",
+        "precio": 1800,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:00hs",
+            "12:00hs",
+            "19:00hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:30hs",
+            "18:30hs"
+          ],
+          "lunes_a_viernes_vuelta": [
+            "07:05hs",
+            "18:35hs"
+          ],
+          "sabados_domingos_vuelta": [
+            "20:50hs"
+          ],
+          "diario": [
+            "14:20hs"
+          ]
+        }
+      },
+      {
+        "destino": "Empalme Ruta 12 Y 18",
+        "precio": 1900,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:00hs",
+            "12:00hs",
+            "19:00hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:30hs",
+            "18:30hs"
+          ],
+          "lunes_a_viernes_vuelta": [
+            "06:45hs",
+            "18:15hs"
+          ],
+          "sabados_domingos_vuelta": [
+            "20:30hs"
+          ],
+          "diario": [
+            "14:00hs"
+          ]
+        }
+      },
+      {
+        "destino": "Sauce Montrul",
+        "precio": 2000,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:00hs",
+            "12:00hs",
+            "19:00hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:30hs",
+            "18:30hs"
+          ],
+          "lunes_a_viernes_vuelta": [
+            "06:40hs",
+            "18:10hs"
+          ],
+          "sabados_domingos_vuelta": [
+            "20:25hs"
+          ],
+          "diario": [
+            "13:55hs"
+          ]
+        }
+      },
+      {
+        "destino": "Acceso Escuela Almafuerte",
+        "precio": 2500,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:00hs",
+            "12:00hs",
+            "19:00hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:30hs",
+            "18:30hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Escuela Almafuerte",
+        "precio": 2500,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:00hs",
+            "12:00hs",
+            "19:00hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:30hs",
+            "18:30hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "La Picada",
+        "precio": 2700,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:00hs",
+            "12:00hs",
+            "19:00hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:30hs",
+            "18:30hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Colonia Nueva",
+        "precio": 3000,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:00hs",
+            "12:00hs",
+            "19:00hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:30hs",
+            "18:30hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "La Balsa",
+        "precio": 3900,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:00hs",
+            "12:00hs",
+            "19:00hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:30hs",
+            "18:30hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Escuela Agrotecnica",
+        "precio": 4600,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:00hs",
+            "12:00hs",
+            "19:00hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:30hs",
+            "18:30hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Villa Urquiza",
+        "precio": 4700,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:00hs",
+            "12:00hs",
+            "19:00hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:30hs",
+            "18:30hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      }
     ]
   },
   {
-    origen: "San Benito",
-    destinos: [
-      { destino: "C Avellaneda", hora_salida: "08:40", hora_llegada: "09:00", precio: 1400 },
-      { destino: "Sauce Montrul", hora_salida: "08:40", hora_llegada: "09:15", precio: 2700 },
-      { destino: "Picadita", hora_salida: "08:40", hora_llegada: "09:30", precio: 4000 },
-      { destino: "La Balsa", hora_salida: "08:40", hora_llegada: "09:40", precio: 5000 },
-      { destino: "Urquiza", hora_salida: "08:40", hora_llegada: "10:00", precio: 7000 }
+    "origen": "San Benito",
+    "destinos": [
+      {
+        "destino": "Colonia Avellaneda",
+        "precio": 1500,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:05hs",
+            "12:05hs",
+            "19:05hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:35hs",
+            "18:35hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Empalme Ruta 12 Y 18",
+        "precio": 1500,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:05hs",
+            "12:05hs",
+            "19:05hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:35hs",
+            "18:35hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Sauce Montrul",
+        "precio": 1500,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:05hs",
+            "12:05hs",
+            "19:05hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:35hs",
+            "18:35hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Acceso Escuela Almafuerte",
+        "precio": 1700,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:05hs",
+            "12:05hs",
+            "19:05hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:35hs",
+            "18:35hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Escuela Almafuerte",
+        "precio": 1700,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:05hs",
+            "12:05hs",
+            "19:05hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:35hs",
+            "18:35hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "La Picada",
+        "precio": 1900,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:05hs",
+            "12:05hs",
+            "19:05hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:35hs",
+            "18:35hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Colonia Nueva",
+        "precio": 2500,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:05hs",
+            "12:05hs",
+            "19:05hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:35hs",
+            "18:35hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "La Balsa",
+        "precio": 2900,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:05hs",
+            "12:05hs",
+            "19:05hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:35hs",
+            "18:35hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Escuela Agrotecnica",
+        "precio": 3300,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:05hs",
+            "12:05hs",
+            "19:05hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:35hs",
+            "18:35hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Villa Urquiza",
+        "precio": 3500,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:05hs",
+            "12:05hs",
+            "19:05hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:35hs",
+            "18:35hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      }
     ]
   },
   {
-    origen: "C Avellaneda",
-    destinos: [
-      { destino: "Sauce Montrul", hora_salida: "09:00", hora_llegada: "09:15", precio: 1300 },
-      { destino: "Picadita", hora_salida: "09:00", hora_llegada: "09:30", precio: 2600 },
-      { destino: "La Balsa", hora_salida: "09:00", hora_llegada: "09:40", precio: 3600 },
-      { destino: "Urquiza", hora_salida: "09:00", hora_llegada: "10:00", precio: 5600 }
-    ]
-  },
-  {
-    origen: "Sauce Montrul",
-    destinos: [
-      { destino: "Picadita", hora_salida: "09:15", hora_llegada: "09:30", precio: 1300 },
-      { destino: "La Balsa", hora_salida: "09:15", hora_llegada: "09:40", precio: 2300 },
-      { destino: "Urquiza", hora_salida: "09:15", hora_llegada: "10:00", precio: 4300 }
-    ]
-  },
-  {
-    origen: "Picadita",
-    destinos: [
-      { destino: "La Balsa", hora_salida: "09:30", hora_llegada: "09:40", precio: 1000 },
-      { destino: "Urquiza", hora_salida: "09:30", hora_llegada: "10:00", precio: 2300 }
-    ]
-  },
-  {
-    origen: "La Balsa",
-    destinos: [
-      { destino: "Urquiza", hora_salida: "09:40", hora_llegada: "10:00", precio: 2000 }
+    "origen": "Colonia Avellaneda",
+    "destinos": [
+      {
+        "destino": "Empalme Ruta 12 Y 18",
+        "precio": 1500,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:10hs",
+            "12:10hs",
+            "19:10hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:40hs",
+            "18:40hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Sauce Montrul",
+        "precio": 1500,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:10hs",
+            "12:10hs",
+            "19:10hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:40hs",
+            "18:40hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Acceso Escuela Almafuerte",
+        "precio": 1500,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:10hs",
+            "12:10hs",
+            "19:10hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:40hs",
+            "18:40hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Escuela Almafuerte",
+        "precio": 1700,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:10hs",
+            "12:10hs",
+            "19:10hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:40hs",
+            "18:40hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "La Picada",
+        "precio": 1900,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:10hs",
+            "12:10hs",
+            "19:10hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:40hs",
+            "18:40hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Colonia Nueva",
+        "precio": 1900,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:10hs",
+            "12:10hs",
+            "19:10hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:40hs",
+            "18:40hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "La Balsa",
+        "precio": 3300,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:10hs",
+            "12:10hs",
+            "19:10hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:40hs",
+            "18:40hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Escuela Agrotecnica",
+        "precio": 3500,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:10hs",
+            "12:10hs",
+            "19:10hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:40hs",
+            "18:40hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      },
+      {
+        "destino": "Villa Urquiza",
+        "precio": 3600,
+        "horarios": {
+          "lunes_a_viernes_ida": [
+            "6:10hs",
+            "12:10hs",
+            "19:10hs"
+          ],
+          "sabados_domingos_ida": [
+            "10:40hs",
+            "18:40hs"
+          ],
+          "lunes_a_viernes_vuelta": [],
+          "sabados_domingos_vuelta": [],
+          "diario": []
+        }
+      }
     ]
   }
 ];
-
-// Función para normalizar texto
+// 🔤 Normalizar texto
 function limpiarTexto(texto) {
   return texto.toLowerCase()
-    .normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+    .normalize("NFD").replace(/[̀-ͯ]/g, "")
     .replace(/[-_,()]/g, ' ')
     .replace(/\s+/g, ' ')
     .trim();
 }
 
-// Endpoint raíz
+// 🏠 GET raíz
 app.get('/', (req, res) => {
-  res.send('¡API de colectivos funcionando con nuevo formato!');
+  res.send('🚌 API de colectivos funcionando con horarios completos');
 });
 
-// 🔥 NUEVO: Filtrar por origen y destino vía query params
+// 🔍 GET /rutas?origen=X&destino=Y
 app.get('/rutas', (req, res) => {
   const origenParam = limpiarTexto(req.query.origen || "");
   const destinoParam = limpiarTexto(req.query.destino || "");
 
   if (!origenParam || !destinoParam) {
-    return res.json(rutas); // Si no hay filtros, se devuelven todas las rutas
+    return res.json(rutas);
   }
 
   const ruta = rutas.find(r => limpiarTexto(r.origen) === origenParam);
-  if (!ruta) {
-    return res.status(404).json({ mensaje: "Origen no encontrado" });
-  }
+  if (!ruta) return res.status(404).json({ mensaje: "Origen no encontrado" });
 
-  const destino = ruta.destinos.find(d => limpiarTexto(d.destino) === destinoParam);
-  if (!destino) {
-    return res.status(404).json({ mensaje: "Destino no encontrado para ese origen" });
-  }
+  const destino = ruta.destinos.find(d => limpiarTexto(d.destino).includes(destinoParam));
+  if (!destino) return res.status(404).json({ mensaje: "Destino no encontrado para ese origen" });
 
-  // Solo devuelve la ruta encontrada
-  return res.json([
-    {
-      origen: ruta.origen,
-      destinos: [destino]
-    }
-  ]);
+  return res.json([{ origen: ruta.origen, destinos: [destino] }]);
 });
 
-// Opcional: obtener todos los destinos desde un origen
+// 🔁 GET /rutas/:origen (todos los destinos desde una ciudad)
 app.get('/rutas/:origen', (req, res) => {
   const origenParam = limpiarTexto(req.params.origen);
   const ruta = rutas.find(r => limpiarTexto(r.origen) === origenParam);
 
-  if (!ruta) {
-    return res.status(404).json({ mensaje: "Origen no encontrado" });
-  }
+  if (!ruta) return res.status(404).json({ mensaje: "Origen no encontrado" });
 
   res.json(ruta.destinos);
 });
 
-// Iniciar servidor
+// 🚀 Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`Servidor activo en http://localhost:${PORT}`);
 });
